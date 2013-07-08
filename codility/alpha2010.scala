cc9c53eaa571cec70ead7df44688d6b7079f0296
Josh Mahonin <jmahonin@gmail.com>
2013-07-08 13:39:09 -0400

    Add a Scala solution to the Codility Alpha 2010 challenge

---
+++ codility/alpha2010.scala
@@ -0,0 +1,15 @@
+import scala.collection.mutable.Set
+
+object Solution {
+    def solution(A: Array[Int]): Int = {
+        val checkSet: Set[Int] = Set()
+        var prefix = 0;
+        for (i <- A.indices) {
+        	if ( !checkSet.contains(A(i)) ) { checkSet += A(i); prefix = i; }
+        }
+        return prefix;
+    }
+}
+
+println(Solution.solution(Array(1, 2, 3, 1, 2, 3)))
+
