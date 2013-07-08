95758bc19d888c12b7cce273a871555e640d804b
Josh Mahonin <jmahonin@gmail.com>
2013-07-08 14:08:36 -0400

    Fix functional Alpha2010 implementation

--- codility/alpha2010-fun.scala
+++ codility/alpha2010-fun.scala
@@ -3,18 +3,18 @@ import scala.annotation.tailrec
 object Solution {
     def solution(A: Array[Int]): Int = {
     	@tailrec
-    	def solutionHelper(A: Array[Int], S: Set[Int], i: Int): Int = {
-    		if (A.isEmpty) return i;
+    	def solutionHelper(A: Array[Int], S: Set[Int], i: Int, prefix: Int): Int = {
+    		if (A.isEmpty) return prefix;
 
     		if (!S(A.head)) {
-    			solutionHelper(A.tail, S + A.head, i+1)
+    			solutionHelper(A.tail, S + A.head, i+1, i+1)
     		}
     		else {
-    			solutionHelper(A.tail, S, i);
+    			solutionHelper(A.tail, S, i+1, prefix);
     		}
     	}
-    	return solutionHelper(A, Set(), 0) - 1;
+    	return solutionHelper(A, Set(), -1, -1);
     }
 }
 
-println(Solution.solution(Array(1, 2, 3, 1, 2, 3)))
+println(Solution.solution(Array(1,2,3,1,2,3)))
