6e79a3a22cd15870da20dfda3758bcce16ef1da9
Josh Mahonin <jmahonin@gmail.com>
2013-07-10 23:34:03 -0400

    Fix formatting

--- codility/alpha2010-imp.scala
+++ codility/alpha2010-imp.scala
@@ -1,16 +1,16 @@
 import scala.collection.mutable.Set
 
 object Solution {
-    def solution(A: Array[Int]): Int = {
-        val checkSet: Set[Int] = Set()
-        var prefix = 0;
-        // Loop through each element, check if it's in the set
-        // If not, add and mark the index
-        for (i <- A.indices) {
-        	if ( !checkSet.contains(A(i)) ) { checkSet += A(i); prefix = i; }
-        }
-        return prefix;
+  def solution(A: Array[Int]): Int = {
+    val checkSet: Set[Int] = Set()
+    var prefix = 0;
+    // Loop through each element, check if it's in the set
+    // If not, add and mark the index
+    for (i <- A.indices) {
+    	if ( !checkSet.contains(A(i)) ) { checkSet += A(i); prefix = i; }
     }
+    return prefix;
+  }
 }
 
 println(Solution.solution(Array(1, 2, 3, 1, 2, 3)))
